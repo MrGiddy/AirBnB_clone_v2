8fefe4482a5def1a912ecc63e10b305d460a5937
Matumbai Binale <binalematumbai@gmail.com>
2024-05-19 03:08:16 +0300

    (task 10): Set up DBStorage - Amenity

--- models/place.py
+++ models/place.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, INTEGER, FLOAT, ForeignKey
+from sqlalchemy import Column, String, INTEGER, FLOAT, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from models.amenity import Amenity
 
+
+place_amenity = Table(
+    'place_amenity',
+    Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+)
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -20,7 +28,14 @@ class Place(BaseModel, Base):
     price_by_night = Column(INTEGER, default=0, nullable=False)
     latitude = Column(FLOAT, nullable=True)
     longitude = Column(FLOAT, nullable=True)
+
     reviews = relationship('Review', cascade='all, delete', backref='place')
+    amenities = relationship(
+        'Amenity',
+        secondary='place_amenity',
+        viewonly=False
+    )
+
     amenity_ids = []
 
     # For FileStorage relationship between Place and Review
@@ -34,3 +49,25 @@ class Place(BaseModel, Base):
 
         # Filter to get places with ids matching the current place's id
         return [plc for plc in all_places.values() if plc.place_id == self.id]
+
+    @property
+    def amenities(self):
+        """
+        Retrieves all Amenity instances that are linked to the current Place
+        """
+        from models import storage
+
+        all_amenities = storage.all(Amenity)
+        filtered = []
+        for amenity in all_amenities.values():
+            if amenity.id in self.amenity_ids:
+                filtered.append(amenity)
+        return filtered
+
+    @amenities.setter
+    def amenities(self, obj):
+        """Adds an id of an Amenity to the list amenity_ids"""
+
+        if isinstance(obj, Amenity):
+            if obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
